851272132a0371ff2c2553dae34319639a4d8141
Matyas Csiki <matyas.csiki@gmail.com>
2016-02-01 16:43:39 +0100

    gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,28 @@
 
 var jshint = require('gulp-jshint');
 var gulp = require('gulp');
-var watch = require('gulp-watch');
+// var watch = require('gulp-watch');
 var jasmine = require('gulp-jasmine');
 
+gulp.task('default', ['watch']);
 
-gulp.task('lint', function() {
-    gulp.src('lufthansa_calorie_app/**/*.js')
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(jshint.reporter('fail'));
+
+
+gulp.task('jshint', function() {
+  return gulp.src('lufthansa_calorie_app/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
 });
-
 	
 gulp.task('jasmine-test', function () {
   return gulp.src('lufthansa_calorie_app/**/*.js')
     .pipe(jasmine());
 });
 
- 
-gulp.task('watch-files', function () {
-	gulp.watch(['lufthansa_calorie_app/**/*.js'], ['jasmine-test']);
+gulp.task('watch', function() {
+  gulp.watch('lufthansa_calorie_app/**/*.js', ['jshint']);
+});
+
+gulp.task('watch', function() {
+  gulp.watch('lufthansa_calorie_app/**/*.js', ['jasmine-test']);
 });
