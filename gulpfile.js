d2fc6ef282e512515408854d8f8ab7bdc782ff18
Lilla Virag <viraglilla@gmail.com>
2016-02-01 16:16:53 +0100

    gulp: add jshint, watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
-var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var jshint = require('gulp-jshint');
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var jasmine = require('gulp-jasmine');
+
+
+gulp.task('lint', function() {
+    gulp.src('lufthansa_calorie_app/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'))
+        .pipe(notify({
+            title: 'JSHint',
+            message: 'JSHint Passed. Let it fly!',
+        }));
+});
+
+	
+gulp.task('jasmine-test', function () {
+  return gulp.src('lufthansa_calorie_app/**/*.js')
+    .pipe(jasmine());
+});
+
+ 
+gulp.task('watch-jasmine', function () {
+	gulp.watch(['lufthansa_calorie_app/**/*.js'], ['jasmine-test']);
 });
