bb13db759b48b59af091df9d74fef9924f603d4d
glukocerin <ati.csordas@gmail.com>
2016-02-02 08:50:41 +0100

    create a public folder and procfile for heroku and server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, function() {
+	console.log('Listening on port 3000');
+});
+
+app.get('/', function(req, res) {
+	res.send('let me fly!');
+});
