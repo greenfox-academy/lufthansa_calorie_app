6937877024bdf72f87cd1ca32e0359d93e15146d
glukocerin <ati.csordas@gmail.com>
2016-02-02 16:12:31 +0100

    add getOne meal function to server.js

--- server.js
+++ server.js
@@ -10,25 +10,55 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 
-app.get('/meals', function (request, response) {
+app.get('/meals', function (req, res) {
 	getAll(function (err, result) {
 		if (err) {
-		 console.error(err); response.send('Error ' + err);
+		 console.error(err); res.send('Error ' + err);
 		}
 		else {
-		response.json(result.rows);
+		res.json(result.rows);
 		};
 	});
 });
 
-function getAll(callback) {
+function getAll(cb) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		client.query('SELECT * FROM meal_table', function(err, result) {
 			done();
-			callback(err, result);
+			cb(err, result);
 		});
 	});
 };
+
+app.get('/meals/:id', function (req, res) {
+	getOne(req.params.id, function (res) {
+			console.log('response: ', res);
+			res.json(result);
+		}
+	});
+});
+
+
+function getOne (id, cb) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query('SELECT meal_id, name, calorie, date WHERE id= ?', id, function(err, result) {
+			done();
+			if (err) {
+				throw err;	
+			};
+			console.log('result: ', result);
+			return cb({'id': id});
+		});
+	});
+}
+
+
+
+
+
+
+
+
 
 app.listen(port, function() {
 	console.log('app listen on port: ', port)
