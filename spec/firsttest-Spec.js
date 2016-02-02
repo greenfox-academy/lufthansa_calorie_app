f23924519176bf785d2fa3f4e1dab5ecedb7a2c3
glukocerin <ati.csordas@gmail.com>
2016-02-02 10:13:58 +0100

    check test

--- spec/firsttest-Spec.js
+++ spec/firsttest-Spec.js
@@ -1,10 +1,10 @@
 describe("Player", function() {
-  var sayHello = require('../hello-world');
+  var sayHello = require('../public/script/hello-world');
 
   it("should be able to play a Song", function() {
 
     //demonstrates use of custom matcher
-    expect(sayHello()).toEqual('Hell!');
+    expect(sayHello()).toEqual('Hello!');
   });
 
 });
