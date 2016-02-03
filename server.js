73878f8326d9c1037bcb62b979728909a3eebd86
glukocerin <ati.csordas@gmail.com>
2016-02-03 16:40:17 +0100

    fix server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var db = require('./dataBaserequests.js');
+var db = require('./dataBaseRequests.js');
 var app = express();
 var port = process.env.PORT || 3000;
 
