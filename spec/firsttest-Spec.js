e9aa00e43602e747740b582fd9b05554d708c059
glukocerin <ati.csordas@gmail.com>
2016-02-04 16:33:26 +0100

    first test

--- spec/firsttest-Spec.js
+++ spec/firsttest-Spec.js
@@ -1,9 +1,17 @@
 'use strict';
 
-// describe('test the test', function() {
-// 	var test = require('../dataBaseRequests');
+var Meal = require('../dataBaseRequests');
 
-// 	it('should give back Hello World', function() {
-// 		expect(test.helloWorld()).toEqual('hello world');
-// 	});
-// });
+describe('meal', function() {
+	it('should give back one item', function() {
+	  function query(query, cb) {
+		  cb(null, [{}, {}]);
+	  }
+		
+	  var meal = new Meal(query);
+
+		meal.getAll(function(err, meals) {
+			expect(meals.length).toEqual(2);
+		});
+	});
+});
