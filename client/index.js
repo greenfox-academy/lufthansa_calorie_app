328cb34e82e5c67db45074f997580420b62d3521
Matyas Csiki <matyas.csiki@gmail.com>
2016-02-10 13:21:45 +0100

    meal listing on page load

--- client/index.js
+++ client/index.js
@@ -1,10 +1,9 @@
 'use strict';
-
 var React = require('react');
 var ReactDOM = require('react-dom');
 import FlatButton from 'material-ui/lib/flat-button';
 
-var url = 'http://localhost:3000/meals';
+var url = 'https://calorie-app8.herokuapp.com/' || 'http://localhost:3000/meals';
 
 const FlatButtonExampleSimple = () => (
   <div>
@@ -12,26 +11,44 @@ const FlatButtonExampleSimple = () => (
   </div>
 );
 
+var createRequest = function(method, url, data, cb) {
+  var request = new XMLHttpRequest();
+  request.open(method, url, true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(data);
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      cb(request.response);
+    }
+  }
+}
+
+var MealList = React.createClass({
+  render: function(response) {
+    var createItem = function(meal) {
+      return <li>{meal.name} {meal.calorie}kCal {meal.date}</li>;
+    };
+    return <ul>{this.props.items.map(createItem)}</ul>;
+  }
+})
+
 var CalorieInput = React.createClass({
 
-  createRequest: function(method, url, data, cb) {
-    var request = new XMLHttpRequest();
-    request.open(method, url, true);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(data);
-    request.onreadystatechange = function() {
-      if (request.readyState === 4) {
-        cb(request.response);
-      }
-    }
-  },
-
-  requestChecker: function(data) {
-    console.log(data);
-  },
-
   getInitialState: function() {
-    return {name: '', calorie: '', date: Date.now()};
+    return {items: [], name: '', calorie: '', date: Date.now()};
+  },
+
+  updateList: function(response) {
+    console.log(response);
+    this.setState({items: JSON.parse(response)})
+  },
+
+  componentDidMount: function() {
+    this.getItems();
+  },
+
+  getItems: function() {
+  createRequest('GET', url, {}, this.updateList);
   },
 
   mealOnChange: function(event) {
@@ -54,27 +71,23 @@ var CalorieInput = React.createClass({
       date: this.state.date
     };
     var data = JSON.stringify(dataToObject);
-    // this.requestChecker('')
+    createRequest('POST', url, data, getItems);
 
-    this.createRequest('POST', url, data, this.requestChecker)
-    // console.log(data);
-    // console.log('lufi');
   },
-
   render: function() {
     return (
       <div>
         <h1>Calorie Counter</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.mealOnChange} value={this.state.name} placeholder="meal" />
-          <input onChange={this.calorieOnChange} value={this.state.calorie} placeholder="calorie" />
-          <input type="datetime-local" onChange={this.dateOnChange} value={this.state.date} />
-          <button>SUBMIT</button>
-        </form>
+          <form onSubmit={this.handleSubmit}>
+            <input onChange={this.mealOnChange} value={this.state.name} placeholder="meal" />
+            <input onChange={this.calorieOnChange} value={this.state.calorie} placeholder="calorie" />
+            <input type="datetime-local" onChange={this.dateOnChange} value={this.state.date} />
+            <button>SUBMIT</button>
+          </form>
+        <MealList items={this.state.items}/>
       </div>
       );
   }
-
 });
 
 ReactDOM.render(<CalorieInput />, document.getElementById('app'))
