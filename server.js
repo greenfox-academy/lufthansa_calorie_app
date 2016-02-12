b1d6004ed7980225c514191fb55ede691ce46474
glukocerin <ati.csordas@gmail.com>
2016-02-12 14:35:58 +0100

    refactor

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var initExpress = require('./server/initExpress.js');
 
 var dataBaseQuery = require('./server/dataBaseQuery');
 var app = initExpress(dataBaseQuery);
-var port = process.env.PORT || config.localPort;
+var port = config.PORT_FOR_SERVER;
 
 app.listen(port, function() {
 	console.log('Listening on port: ' + port);
